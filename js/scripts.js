bf8afbc25ae4c7bc95313a68ebe7213bbcf0345f
Student <example@example.com>
2019-09-09 20:05:09 -0700

    added js, css, html

---
+++ js/scripts.js
@@ -0,0 +1,11 @@
+$(document).ready(function() {
+  $('#blanks form').submit(function(event) {
+    var person1Input = $("input#person1").val();
+
+      $(".person1").text(person1Input);
+
+      $("#story").show();
+
+      event.preventDefault();
+    });
+});
